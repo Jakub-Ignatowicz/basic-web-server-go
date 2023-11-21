300ed3a108a662b320465d96f22819975a108131
Jakub-Igntaowicz <kuba_ign@proton.me>
2023-11-21 15:47:24 +0100

    Added /hello route and implemented simple error handling

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at porst 8080\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
